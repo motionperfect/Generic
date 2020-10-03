c9935779dd137d960662e749c60e097785f6e0eb
Julien Sarriot <julien.sarriot@epitech.eu>
2020-10-03 20:58:08 +0200

    fix "schema.ts" io import

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -2,6 +2,6 @@ import * as Joi from '@hapi/joi';
 
 import { schema as app } from './app';
 import { schema as security } from './security';
-import { schema as io } from './io';
+import { schema as io } from './IO';
 
 export const schema = Joi.object({ ...app, ...io, ...security });
