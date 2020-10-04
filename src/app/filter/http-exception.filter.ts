98c3e85262a07c8ca0feb98507485402f9d588bf
Julien S <28895006+Goldweavers@users.noreply.github.com>
2020-10-04 23:10:49 +0200

    Update http-exception.filter.ts

--- src/app/filter/http-exception.filter.ts
+++ src/app/filter/http-exception.filter.ts
@@ -3,24 +3,26 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
-  Logger,
-  HttpStatus,
+  BadRequestException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  private readonly logger = new Logger(HttpExceptionFilter.name);
 
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus() || HttpStatus.INTERNAL_SERVER_ERROR;
+    const status = exception.getStatus();
 
-    this.logger.error(exception.message);
+    const errors = exception instanceof BadRequestException
+      ? (exception.getResponse() as any).message
+      : [exception.message];
+
     response.status(status).json({
       statusCode: status,
+      errors: errors,
       timestamp: new Date().toISOString(),
       path: request.url,
     });
