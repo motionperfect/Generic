467f2079fcddc2b20a6823c324661af22123a05b
Julien Sarriot <julien.sarriot@epitech.eu>
2020-10-04 17:51:41 +0200

    Added validation modules

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 import { AppModule } from './app/app.module';
 
@@ -7,6 +9,20 @@ import { AppConfigService } from './config/app';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfigService = app.get(AppConfigService);
+
+  /**
+   * This will cause class-validator to use the nestJS module resolution,
+   * the fallback option is to spare our self from importing all the
+   * class-validator modules to nestJS
+   */
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidUnknownValues: true,
+    }),
+  );
 
   await app.listen(appConfigService.port);
 }
